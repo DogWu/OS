1f556f22adfd6cf482072cb09780214f9feb02f5
suchfun <846952972@qq.com>
2017-02-10 17:08:06 +0800

    使用C指针代替汇编实现的函数

--- bootpack.c
+++ bootpack.c
@@ -2,8 +2,9 @@ void io_hlt(void);
 void write_mem8(int addr,int data);
 void HariMain(void){
 	int i;
-	for(i=0xa0000;i<=0xaffff;i++){
-		write_mem8(i,15);
+	char *p=0xa0000;
+	for(i=0x0000;i<=0xffff;i++){
+		*(p+i)=i&0x0f;
 	}
 	for(;;)	io_hlt();
 }
